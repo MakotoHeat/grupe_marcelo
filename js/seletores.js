1626bfb0f838ac08372ee651eb4eb1b21327b988
MakotoHeat <mjfmarcelo@gmail.com>
2023-04-06 11:48:42 -0300

    Aula Javascript 1

---
+++ js/seletores.js
@@ -0,0 +1,16 @@
+let frase = document.getElementById('colegas-classe')
+
+let nomeCaique = document.getElementById("amigo")
+nomeCaique.innerHTML = "Caiqueee"
+
+
+let nomeJéssica = document.getElementById ("inimigo")
+nomeJéssica.innerHTML = "Jéssicaaa"
+
+let nomeRodrigo = document.getElementById ("desconhecido")
+nomeRodrigo.innerHTML = "Rodrigooo"
+
+let Frase = document.getElementById ("Frase-motivacional")
+Frase.innerHTML = "Frase motivacional"
+
+let 
---
+++ js/seletores1.js
@@ -0,0 +1,6 @@
+
+
+
+
+
+<script type="text/javascript" src=" ./js/seletores1.js"></script>
